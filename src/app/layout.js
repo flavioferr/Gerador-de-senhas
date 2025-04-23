e2ecacab851a38ae7fed0c7309be822f86fa39e3
Flavio Fernandes <axiimpact89@gmail.com>
2025-04-23 17:43:45 -0300

    site-gerador-de-senha-v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8155657319861390"
+        crossorigin="anonymous"
+      ></script>
       <body className={inter.className}>
         {children}
         <Toaster position="top-right" richColors />
